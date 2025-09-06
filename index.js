4bb2e8b27e931f77fe91d060a07abd897fbc0256
Fatma-Suleiman <97163430+Fatma-Suleiman@users.noreply.github.com>
2025-09-06 08:30:34 +0300

    second commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const num1=12;
+const num2=5;
+const name= "Fatma Suleiman"
+
+const addition = num1 + num2;
+const subtraction = num1 - num2;
+const multiplication = num1 * num2;
+const division = num1 / num2;
+
+console.log('Addition:', addition);
+console.log('Subtraction:', subtraction);
+console.log('Multiplication:', multiplication);
+console.log('Division:', division);
+
+console.log(`Hello ${name}, the result of ${num1} + ${num2} is ${addition}`);
